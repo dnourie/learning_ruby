64b9fb1f28b6c5d430e1e52cbfdd0a6a915586f3
dnourie <dananourie@gmail.com>
2014-08-16 17:36:15 -0700

    finished exercises

---
+++ exercises/scream.rb
@@ -0,0 +1,15 @@
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+#To make it print
+
+def scream(words)
+  words + "!!!!!"
+end
+
+puts scream("Yippeeee")
