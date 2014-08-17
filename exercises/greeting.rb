5b445dcfe5ba57d3a3d659aaa0befe86be414adb
dnourie <dananourie@gmail.com>
2014-08-16 17:20:26 -0700

    more exercises

---
+++ exercises/greeting.rb
@@ -0,0 +1,7 @@
+
+def greeting(name)
+  "Good day to you, #{name}!"
+end
+
+puts greeting("Dana")
+
--- exercises/name.rb
+++ exercises/name.rb
@@ -1,11 +1,13 @@
 
 
-puts "Please type in your name." 
-name = gets.chomp
-puts greeting = "Hello, nice to meet you, #{name}!"
+puts "Please type in your first name:" 
+first_name = gets.chomp
+puts "Now type your last name:"
+last_name = gets.chomp
+puts greeting = "Hello, nice to meet you, #{first_name} #{last_name}!"
 
 10.times do |n|
-  puts name
+  puts first_name
 end
 
 
